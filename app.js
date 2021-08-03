38cab287686e1103bc5578f9b7fd2e52a5ae7444
muhannedalogaidi <muhannedalogaidi@gmail.com>
2021-08-03 01:53:31 -0400

    Create the server and listen to poret 3000 add middleware to listen to any incomming request (which is use) Add the start command to package.json and run the application and test

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express(); // This is a top level function 
+
+// Middleware section
+
+app.use((req,res,next)=>{
+    console.log(req)
+});
+
+app.listen(3000)
+
