a626e454116aa1cba33cb22f890ee1fba967614b
muhannedalogaidi <muhannedalogaidi@gmail.com>
2021-08-03 23:08:14 -0400

    Add use middleware and test the next for any incoming request

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express(); // This is a top level function
 // Middleware section
 
 app.use((req,res,next)=>{
-    console.log(req)
+    console.log('First middleware');
+    next(); // Allow middleware to continue to next middleware
+});
+
+app.use((req,res,next)=>{
+    console.log('Second middleware');
+    res.send('<h3>Hello PIS Users</h3>')
 });
 
 app.listen(3000)
