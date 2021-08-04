b1200b999905c1abba23fd4339af4ecaed612378
muhannedalogaidi <muhannedalogaidi@gmail.com>
2021-08-04 00:52:01 -0400

    Install body parser  and test it

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+
 const app = express(); // This is a top level function 
 
+app.use(bodyparser.urlencoded({ extended: false }));
 // Middleware section
 
-app.use((req,res,next)=>{
-    console.log('First middleware');
-    next(); // Allow middleware to continue to next middleware
+app.use('/add-Card', (req, res, next) => {
+    res.send('<form action="/card" method="POST"><input type="text" name="title"><button type="submit">Add Card</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log('Second middleware');
-    res.send('<h3>Hello PIS Users</h3>')
+app.use('/card', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-app.listen(3000)
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from PIS!</h1>');
+});
+
+
+app.listen(3000);
 
