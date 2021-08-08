41cb60c5bfd3999c2abe1db8166d54c77deff3f1
muhannedalogaidi <muhannedalogaidi@gmail.com>
2021-08-08 01:40:57 -0400

    Add EJS engine for views and define it under express install EJS convert the views to template and the file extention goes to be .ejs in route now use render to render the ejs files

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express(); // This is a top level function 
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRouter = require('./routes/admin');
 const cardRouter= require('./routes/card');
 const homeRouter= require('./routes/home');
@@ -13,9 +16,10 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname,'public')));
 
 // Routing Middleware section
-app.use(adminRouter);
+app.use('/admin', adminRouter.routers);
 app.use(cardRouter);
 app.use(homeRouter);
+
 app.use((req,res,next)=>{
     res.status(404).send("<h1>Page not found</h1>")
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const path = require('path');
 
-const router = express.Router();
+const routers = express.Router();
+const cards = [];
 
-router.get('/add-card',(req,res,next)=>{
+// /admin/add-card => GET
+routers.get('/add-card', (req, res, next) => {
+    res.render('add-card', {
+      pageTitle: 'Add card',
+      path: '/admin/add-card',
+      formsCSS: true,
+      cardCSS: true,
+      activeAddcard: true
+    });
+  });
+  
+  // /admin/add-card => POST
+  routers.post('/xcv', (req, res, next) => {
+    cards.push({ card_name: req.body.card_name });
+    res.redirect('/');
+  });
+  
+ 
+/* routers.get('/add-card',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'../','views','admin.html'));
-});
+}); */
 
-module.exports = router;
+exports.routers = routers;
+exports.cards = cards;
